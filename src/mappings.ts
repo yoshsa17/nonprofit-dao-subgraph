0ff6df907955fc4f2ba496b85fc0fd7d7e24664a
Yoshiro Saito <saiorsin44den@gmail.com>
2022-07-09 23:28:28 +0900

    chore: format code

--- src/mappings.ts
+++ src/mappings.ts
@@ -2,10 +2,10 @@ import { BigInt, Address , store, Bytes, ByteArray, log} from "@graphprotocol/gr
 import {
   ProposalCreated as ProposalCreatedEvent,
   VoteCast as VoteCastEvent,
-	ProposalCanceled as ProposalCanceledEvent,
-	ProposalExecuted as ProposalExecutedEvent,
-	EtherSent as EtherSentEvent,
-	EtherDeposited as EtherDepositedEvent,
+  ProposalCanceled as ProposalCanceledEvent,
+  ProposalExecuted as ProposalExecutedEvent,
+  EtherSent as EtherSentEvent,
+  EtherDeposited as EtherDepositedEvent,
   ApprovedToDomain as ApprovedToDomainEvent,
   RevokedDomainAllowance as RevokedDomainAllowanceEvent,
 } from "../generated/GovernorNPO/GovernorNPO";
@@ -15,22 +15,22 @@ import {
   AddedNewRole as AddedNewRoleEvent,
   RoleGranted as RoleGrantedEvent,
   RoleAdminChanged as RoleAdminChangedEvent,
-	SetEvaluationRound as SetEvaluationRoundEvent,
+  SetEvaluationRound as SetEvaluationRoundEvent,
   Burned as BurnedEvent,
   RoleRevoked as RoleRevokedEvent,
-  // SBRTManager as SBRTManagerContract,
+  SBRTManager as SBRTManagerContract,
 } from "../generated/SBRTManager/SBRTManager";
 
 import {
-	Member,
+  Member,
   Token,
-	Reputation,
+  Reputation,
   Role,
   Role_Member,
-	EvaluationRound,
-	Proposal,
-	Transaction,
-	Vote,
+  EvaluationRound,
+  Proposal,
+  Transaction,
+  Vote,
 } from "../generated/schema";
 
 // -----------------------------------------------------------
@@ -38,81 +38,81 @@ import {
 // -----------------------------------------------------------
 
 export function handleProposalCreated(event: ProposalCreatedEvent): void {
-	let proposal = new Proposal(event.params.proposalId.toString());
+  let proposal = new Proposal(event.params.proposalId.toString());
   let member = Member.load(event.params.proposer.toHexString());
   if(member == null) return;
-	proposal.proposer = event.params.proposer.toHexString();
-	proposal.description = event.params.description;
-	proposal.targets = event.params.targets.map<string>(x => x.toHexString());
-	proposal.values = event.params.values;
-	proposal.calldatas = event.params.calldatas;
-	proposal.startBlock = event.params.startBlock;
-	proposal.endBlock = event.params.endBlock;
-	proposal.forVotes = BigInt.fromI32(0);
-	proposal.againstVotes = BigInt.fromI32(0);
-	proposal.abstainVotes = BigInt.fromI32(0);
+  proposal.proposer = event.params.proposer.toHexString();
+  proposal.description = event.params.description;
+  proposal.targets = event.params.targets.map<string>(x => x.toHexString());
+  proposal.values = event.params.values;
+  proposal.calldatas = event.params.calldatas;
+  proposal.startBlock = event.params.startBlock;
+  proposal.endBlock = event.params.endBlock;
+  proposal.forVotes = BigInt.fromI32(0);
+  proposal.againstVotes = BigInt.fromI32(0);
+  proposal.abstainVotes = BigInt.fromI32(0);
   member.pCount = member.pCount++;
   member.save();
-	proposal.save();
+  proposal.save();
 }
 
 export function handleVoteCast(event: VoteCastEvent): void {
-	let vote = new Vote(event.transaction.hash.toHexString());
-	vote.voter = event.params.voter.toHexString();
-	vote.proposal = event.params.proposalId.toString();
-	vote.support = event.params.support;
-	vote.weight = event.params.weight;
-	vote.reason = event.params.reason;
-	vote.timestamp = event.block.timestamp;
-	let proposal = Proposal.load(event.params.proposalId.toHexString());
-	if (proposal == null) return;
-	if (event.params.support == 0) {
-		proposal.againstVotes.plus(event.params.weight);
-	}
-	if (event.params.support == 1) {
-		proposal.forVotes.plus(event.params.weight);
-	}
-	if (event.params.support == 2) {
-		proposal.abstainVotes.plus(event.params.weight);
-	}
+  let vote = new Vote(event.transaction.hash.toHexString());
+  vote.voter = event.params.voter.toHexString();
+  vote.proposal = event.params.proposalId.toString();
+  vote.support = event.params.support;
+  vote.weight = event.params.weight;
+  vote.reason = event.params.reason;
+  vote.timestamp = event.block.timestamp;
+  let proposal = Proposal.load(event.params.proposalId.toHexString());
+  if (proposal == null) return;
+  if (event.params.support == 0) {
+    proposal.againstVotes.plus(event.params.weight);
+  }
+  if (event.params.support == 1) {
+    proposal.forVotes.plus(event.params.weight);
+  }
+  if (event.params.support == 2) {
+    proposal.abstainVotes.plus(event.params.weight);
+  }
   let member = Member.load(event.params.voter.toHexString());
   if(member == null) return;
   member.vCount = member.vCount++;
-	vote.save();
-	proposal.save();
+  vote.save();
+  proposal.save();
 }
 
 export function handleProposalCanceled(event: ProposalCanceledEvent): void {
-	let proposal = Proposal.load(event.params.proposalId.toHexString());
-	if (proposal == null) return;
-	proposal.executed = true;
-	proposal.save();
+  let proposal = Proposal.load(event.params.proposalId.toHexString());
+  if (proposal == null) return;
+  proposal.executed = true;
+  proposal.save();
 }
 
 export function handleProposalExecuted(event: ProposalExecutedEvent): void {
-	let proposal = Proposal.load(event.params.proposalId.toHexString());
-	if (proposal == null) return;
-	proposal.canceled = true;
-	proposal.save();
+  let proposal = Proposal.load(event.params.proposalId.toHexString());
+  if (proposal == null) return;
+  proposal.canceled = true;
+  proposal.save();
 }
 
 export function handleEtherSent(event: EtherSentEvent): void {
-	let transaction = new Transaction(event.transaction.hash.toHexString());
-	transaction.timestamp = event.block.timestamp;
-	transaction.isIncoming = false;
-	transaction.srcOrDst = event.params.to.toHexString();
-	transaction.amount = event.params.amount;
-	transaction.reference = event.params.reason;
-	transaction.save();
+  let transaction = new Transaction(event.transaction.hash.toHexString());
+  transaction.timestamp = event.block.timestamp;
+  transaction.isIncoming = false;
+  transaction.srcOrDst = event.params.to.toHexString();
+  transaction.amount = event.params.amount;
+  transaction.reference = event.params.reason;
+  transaction.save();
 }
 
 export function handleEtherDeposited(event: EtherDepositedEvent): void {
-	let transaction = new Transaction(event.transaction.hash.toHexString());
+  let transaction = new Transaction(event.transaction.hash.toHexString());
   transaction.timestamp = event.block.timestamp;
-	transaction.isIncoming = true;
-	transaction.srcOrDst = event.params.from.toHexString();
-	transaction.amount = event.params.amount;
-	transaction.save();
+  transaction.isIncoming = true;
+  transaction.srcOrDst = event.params.from.toHexString();
+  transaction.amount = event.params.amount;
+  transaction.save();
 }
 
 export function handleApprovedToDomain(event: ApprovedToDomainEvent): void {
@@ -158,26 +158,26 @@ export function handleMinted(event: MintedEvent): void {
 
 // TODO: refactor this not to use nullable types of Role entity
 export function handleAddedReputation(event: AddedReputationEvent): void {
-	let reputation = new Reputation(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-	reputation.contributor = event.params.contributor.toHexString();
-	reputation.evaluator = event.params.evaluator.toHexString();
+  let reputation = new Reputation(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  reputation.contributor = event.params.contributor.toHexString();
+  reputation.evaluator = event.params.evaluator.toHexString();
   let domain = Role.load(event.params.domainId.toHexString());
   if(!domain) {
     domain = new Role(event.params.domainId.toHexString());
     domain.save();
   }
   reputation.domain = event.params.domainId.toHexString();
-	reputation.reason = event.params.reason;
-	reputation.timestamp = event.block.timestamp;
-	reputation.evaluationRound = event.params.roundId.toString();
-	reputation.save();
+  reputation.reason = event.params.reason;
+  reputation.timestamp = event.block.timestamp;
+  reputation.evaluationRound = event.params.roundId.toString();
+  reputation.save();
 
-	let receiver = Member.load(event.params.contributor.toHexString());
-	let evaluator = Member.load(event.params.evaluator.toHexString());
-	if (!receiver|| !evaluator) return;
+  let receiver = Member.load(event.params.contributor.toHexString());
+  let evaluator = Member.load(event.params.evaluator.toHexString());
+  if (!receiver|| !evaluator) return;
   receiver.rCount++;
   evaluator.eCount++;
-	receiver.save();
+  receiver.save();
   evaluator.save();
   
   // TODO: update tokenURI for receiver and evaluator
@@ -226,10 +226,10 @@ export function handleAddedNewRole(event: AddedNewRoleEvent): void {
 }
 
 export function handleSetEvaluation(event: SetEvaluationRoundEvent): void {
-	let round = new EvaluationRound(event.params.roundId.toString());
-	round.startBlock = event.params.startBlock;
-	round.endBlock = event.params.endBlock;
-	round.save();
+  let round = new EvaluationRound(event.params.roundId.toString());
+  round.startBlock = event.params.startBlock;
+  round.endBlock = event.params.endBlock;
+  round.save();
 }
 
 export function handleBurned(event: BurnedEvent): void {
